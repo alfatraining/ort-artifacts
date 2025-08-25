b0298b53e2b34afbeaf7ded04824427065f53c24
Raphael Menges <Raphael.Menges@alfatraining.de>
2025-08-25 13:18:34 +0200

    Ready for onnxruntime v1.22.2?

--- src/build.ts
+++ src/build.ts
@@ -10,6 +10,7 @@ const arch: 'x64' | 'arm64' = getArch();
 const platform: 'win32' | 'darwin' | 'linux' = getPlatform();
 
 const TARGET_ARCHITECTURE_TYPE = new EnumType(['x86_64', 'aarch64']);
+const TARGET_ANDROID_ABI_TYPE = new EnumType(['armeabi-v7a', 'arm64-v8a', 'x86_64', 'x86']);
 
 const MACOS_DEPLOYMENT_TARGET = 12.0; // Should be 13.3 according to https://github.com/microsoft/onnxruntime/releases/tag/v1.21.0. But due to internal constraints, we must target 12.
 const IPHONE_DEPLOYMENT_TARGET = 16.0;
@@ -18,6 +19,7 @@ await new Command()
 	.name('ort-artifact')
 	.version('0.1.0')
 	.type('target-arch', TARGET_ARCHITECTURE_TYPE)
+	.type('android-abi', TARGET_ANDROID_ABI_TYPE)
 	.option('-r, --reference <string>', 'Exact branch or tag')
 	.option('-s, --static', 'Build static library')
 	.option('--mt', 'Link with static MSVC runtime')
@@ -31,6 +33,8 @@ await new Command()
 	.option('--iphoneos', 'Target iOS / iPadOS')
 	.option('--iphonesimulator', 'Target iOS / iPadOS simulator')
 	.option('--android', 'Target Android')
+	.option('--android_api <number>', 'Android API', { default: 29 })
+	.option('--android_abi <android_abi:android-abi>', 'Android ABI', { default: "arm64-v8a" })
 	.option('-W, --wasm', 'Compile for WebAssembly (with patches)')
 	.option('--emsdk <version:string>', 'Emsdk version to use for WebAssembly build', { default: '4.0.3' })
 	.action(async (options, ..._) => {
@@ -78,53 +82,93 @@ await new Command()
 		const compilerFlags = [];
 		const args = [];
 
+		// Flags across targets.
 		args.push('-Donnxruntime_CLIENT_PACKAGE_BUILD=ON');
+		args.push('-Donnxruntime_ENABLE_LTO=ON');
+		args.push('-Donnxruntime_BUILD_UNIT_TESTS=OFF');
+		args.push('-Donnxruntime_USE_KLEIDIAI=ON');
 		// args.push('-Donnxruntime_MINIMAL_BUILD=ON');
 		// args.push('-Donnxruntime_EXTENDED_MINIMAL_BUILD=ON');
 		// args.push('-DONNX_MINIMAL_BUILD=ON');
 		
+		// Building for macOS and iOS on macOS.
 		if (platform === 'darwin') {
-			args.push('-Dprotobuf_BUILD_PROTOC_BINARIES=OFF');
+			args.push('-Dprotobuf_BUILD_PROTOC_BINARIES=OFF'); // Use flag across targets?
+
+			// Decide between macOS and iOS.
 			args.push(`-DCMAKE_OSX_DEPLOYMENT_TARGET=${options.iphoneos || options.iphonesimulator ? IPHONE_DEPLOYMENT_TARGET : MACOS_DEPLOYMENT_TARGET}`);
-			if(options.iphoneos) {
-				args.push('-DCMAKE_OSX_SYSROOT=iphoneos');
-			} else if(options.iphonesimulator) {
-				args.push('-DCMAKE_OSX_SYSROOT=iphonesimulator');
-			}
 			if(options.iphoneos || options.iphonesimulator) {
 				args.push('-DCMAKE_TOOLCHAIN_FILE=../cmake/onnxruntime_ios.toolchain.cmake');
+				if(options.iphoneos) {
+					args.push('-DCMAKE_OSX_SYSROOT=iphoneos');
+				} else if(options.iphonesimulator) {
+					args.push('-DCMAKE_OSX_SYSROOT=iphonesimulator');
+				}
+			}
+
+			// Cross-architecture compilation.
+			if (options.arch === 'aarch64') {
+				args.push('-DCMAKE_OSX_ARCHITECTURES=arm64');
+			} else {
+				args.push('-DCMAKE_OSX_ARCHITECTURES=x86_64');
+			}
+
+			// CoreML execution provider.
+			if(options.coreml) {
+			args.push('-Donnxruntime_USE_COREML=ON');
 			}
 		}
 
+		// Building for Linux on Linux.
+		if(platform === 'linux' && !options.android) {
+			// Cross-architecture compilation.
+			if (options.arch === 'aarch64' && arch !== 'arm64') {
+				args.push('-Donnxruntime_CROSS_COMPILING=ON');
+				args.push(`-DCMAKE_TOOLCHAIN_FILE=${join(root, 'toolchains', 'aarch64-unknown-linux-gnu.cmake')}`);
+			}
+		}
+
+		// Building for Android on Linux.
 		// TODO: Add nnapi execution provider.
-		// ANDROID_NDK_HOME and ANDROID_SDK_ROOT are expected to be set.
 		if (platform === 'linux' && options.android) {
-			const android_abi = "arm64-v8a"; // TODO: Argument?
-			const android_api = 29; // TODO: Argument?
+			// ANDROID_NDK_HOME and ANDROID_SDK_ROOT are expected to be set in the environment.
 			const android_ndk_path = Deno.env.get("ANDROID_NDK_HOME");
-			args.push('-Donnxruntime_ENABLE_LTO=ON');
-			args.push(`-DANDROID_PLATFORM=android-${android_api}`);
-			args.push(`-DANDROID_ABI=${android_abi}`);
-			args.push(`-DANDROID_MIN_SDK=${android_api}`);
+			args.push(`-DANDROID_PLATFORM=android-${options.android_api}`);
+			args.push(`-DANDROID_ABI=${options.android_abi}`);
 			args.push('-DANDROID_USE_LEGACY_TOOLCHAIN_FILE=false');
 			args.push(`-DCMAKE_TOOLCHAIN_FILE=${join(android_ndk_path, "build", "cmake", "android.toolchain.cmake")}`);
 		}
 
+		// Building for Windows on Windows.
 		if (platform === 'win32' && options.directml) {
+			// https://github.com/microsoft/onnxruntime/pull/21005
+			compilerFlags.push('_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR');
+
+			// Cross-architecture compilation.
+			if (options.arch === 'aarch64' && arch !== 'arm64') {
+				args.push('-Donnxruntime_CROSS_COMPILING=ON');
+				args.push('-A', 'ARM64');
+				compilerFlags.push('_SILENCE_ALL_CXX23_DEPRECATION_WARNINGS');
+			}
+
+			// DirectML execution provider.
 			args.push('-Donnxruntime_USE_DML=ON');
 		}
-		if (platform === 'darwin' && options.coreml) {
-			args.push('-Donnxruntime_USE_COREML=ON');
-		}
+
+		// XNNpack execution provider.
 		if (options.xnnpack) {
 			args.push('-Donnxruntime_USE_XNNPACK=ON');
 		}
+
+		// WebGPU execution provider.
 		if (options.webgpu) {
 			args.push('-Donnxruntime_USE_WEBGPU=ON');
 			args.push('-Donnxruntime_ENABLE_DELAY_LOADING_WIN_DLLS=OFF');
 			// args.push('-Donnxruntime_USE_EXTERNAL_DAWN=OFF');
 			// args.push('-Donnxruntime_BUILD_DAWN_MONOLITHIC_LIBRARY=ON');
 		}
+
+		// OpenVINO execution provider.
 		if (options.openvino && (platform === 'win32' || platform === 'linux')) {
 			// OpenVINO probably not available on macOS: https://github.com/microsoft/onnxruntime/issues/24273
 			args.push('-Donnxruntime_USE_OPENVINO=ON');
@@ -132,29 +176,8 @@ await new Command()
 			args.push('-Donnxruntime_USE_OPENVINO_CPU=ON');
 			args.push('-Donnxruntime_USE_OPENVINO_NPU=ON');
 			args.push('-Donnxruntime_DISABLE_RTTI=OFF');
-			// args.push('-Donnxruntime_USE_OPENVINO_INTERFACE=ON'); <- Not sure what this does.
+			// args.push('-Donnxruntime_USE_OPENVINO_INTERFACE=ON'); // Not sure what this does.
 		}
-
-		// if (platform === 'darwin') {
-		// 	if (options.arch === 'aarch64') {
-		// 		args.push('-DCMAKE_OSX_ARCHITECTURES=arm64');
-		// 	} else {
-		// 		args.push('-DCMAKE_OSX_ARCHITECTURES=x86_64');
-		// 	}
-		// } else {
-		// 	if (options.arch === 'aarch64' && arch !== 'arm64') {
-		// 		args.push('-Donnxruntime_CROSS_COMPILING=ON');
-		// 		switch (platform) {
-		// 			case 'win32':
-		// 				args.push('-A', 'ARM64');
-		// 				compilerFlags.push('_SILENCE_ALL_CXX23_DEPRECATION_WARNINGS');
-		// 				break;
-		// 			case 'linux':
-		// 				args.push(`-DCMAKE_TOOLCHAIN_FILE=${join(root, 'toolchains', 'aarch64-unknown-linux-gnu.cmake')}`);
-		// 				break;
-		// 		}
-		// 	}
-		// }
 
 		if (platform === 'win32') {
 			args.push(`-DONNX_USE_MSVC_STATIC_RUNTIME=${options.mt ? 'ON' : 'OFF'}`);
@@ -167,14 +190,6 @@ await new Command()
 		if (!options.static) {
 			args.push('-Donnxruntime_BUILD_SHARED_LIB=ON');
 		}
-
-		// https://github.com/microsoft/onnxruntime/pull/21005
-		if (platform === 'win32') {
-			compilerFlags.push('_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR');
-		}
-
-		args.push('-Donnxruntime_BUILD_UNIT_TESTS=OFF');
-		args.push('-Donnxruntime_USE_KLEIDIAI=ON');
 
 		if (compilerFlags.length > 0) {
 			const allFlags = compilerFlags.map(def => `-D${def}`).join(' ');
