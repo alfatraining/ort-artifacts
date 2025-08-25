f209b6658aea83b9a744dca1b78a417ef76e8bee
Raphael Menges <Raphael.Menges@alfatraining.de>
2025-08-25 16:39:32 +0200

    Another attempt for minimal builds.

--- src/build.ts
+++ src/build.ts
@@ -89,8 +89,7 @@ await new Command()
 		args.push('-Donnxruntime_BUILD_UNIT_TESTS=OFF');
 		args.push('-Donnxruntime_USE_KLEIDIAI=ON');
 		args.push('-Donnxruntime_MINIMAL_BUILD=ON');
-		args.push('-Donnxruntime_EXTENDED_MINIMAL_BUILD=ON');
-		args.push('-DONNX_MINIMAL_BUILD=ON');
+        args.push('-Donnxruntime_EXTENDED_MINIMAL_BUILD=ON');
 
 		// Building for macOS and iOS on macOS.
 		if (platform === 'darwin') {
