96f063d7034cc200f22561f7dab80f70ca4a8298
Raphael Menges <Raphael.Menges@alfatraining.de>
2025-08-25 16:23:19 +0200

    Try again minimal build.

--- src/build.ts
+++ src/build.ts
@@ -88,9 +88,9 @@ await new Command()
 		args.push(`-Donnxruntime_ENABLE_LTO=${platform === 'win32' && options.static ? "OFF" : "ON"}`);
 		args.push('-Donnxruntime_BUILD_UNIT_TESTS=OFF');
 		args.push('-Donnxruntime_USE_KLEIDIAI=ON');
-		// args.push('-Donnxruntime_MINIMAL_BUILD=ON');
-		// args.push('-Donnxruntime_EXTENDED_MINIMAL_BUILD=ON');
-		// args.push('-DONNX_MINIMAL_BUILD=ON');
+		args.push('-Donnxruntime_MINIMAL_BUILD=ON');
+		args.push('-Donnxruntime_EXTENDED_MINIMAL_BUILD=ON');
+		args.push('-DONNX_MINIMAL_BUILD=ON');
 
 		// Building for macOS and iOS on macOS.
 		if (platform === 'darwin') {
